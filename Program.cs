444f355768472ea4b405fb38a66afe104bf68d20
AAARRRRR <zhongjye@126.com>
2023-04-03 15:34:04 +0800

    [feat] add insert and delete attempt in main program

--- Program.cs
+++ Program.cs
@@ -23,4 +23,14 @@ foreach (var transaction in transactions)
     Console.WriteLine(transaction.Product.Name);
 }
 
+//Insert
+var newTransaction = new Transaction { ProductId = 2, CustomerId = 2 };
+dbcontext.Add(newTransaction);
+var newProduct = new Product { Name = "salt", Price = 3};
+dbcontext.Add(newProduct);
+
+//Delete
+var removedTransaction = new Transaction { Id = 4, ProductId = 2, CustomerId = 2 };
+dbcontext.Remove(removedTransaction);
+
 dbcontext.SaveChanges();
