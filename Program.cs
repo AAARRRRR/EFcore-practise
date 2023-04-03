424371e29294f02b33432ef3a3dcf835e358a0e5
AAARRRRR <zhongjye@126.com>
2023-04-03 16:26:10 +0800

    [feat] try to use sql connection pool

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 ﻿using EFcore_practise2;
 using EFcore_practise2.Entities;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 var dbcontext = new ECommerceDbContext();
 var dataSeeding =  new DataSeeding(dbcontext);
 
-// Query
+//Query
  var customers = from customer in dbcontext.Customers select customer;
  foreach (var customer in customers)
  {
@@ -14,8 +15,9 @@ var dataSeeding =  new DataSeeding(dbcontext);
 
 //Query2
 var transactions = dbcontext.Transactions
-    .Include("Product")
     .Where(t => t.ProductId == 1)
+    .Include(t => t.Product)
+    .AsSplitQuery()
     .AsNoTracking()
     .ToList();
 foreach (var transaction in transactions)
@@ -38,3 +40,16 @@ foreach (var transaction in transactions)
 // dbcontext.Update(updatedProduct);
 
 dbcontext.SaveChanges();
+
+//use sql connectionPool
+using var connection = new SqlConnection(
+    "Data Source=127.0.0.1,1433;Initial Catalog=master;" +
+    "User Id=sa;Password=MyPass@word;TrustServerCertificate=True;MultiSubnetFailover=True;");
+connection.Open();
+var command =
+    new SqlCommand("INSERT INTO Customers (Name, Email, PhoneNumber) VALUES ('Calvin', 'Calvin@123.com', '333')",connection);
+var command2 =
+    new SqlCommand("INSERT INTO Transactions (ProductId, CustomerId) VALUES (1, 3)",connection);
+command.ExecuteNonQuery();
+command2.ExecuteNonQuery();
+connection.Close();
